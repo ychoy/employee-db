0356bb92bda7dee9031d69b65bac300e65e58074
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-07-11 20:19:51 -0700

    add validations for creating new employee

--- app/assets/javascripts/components/employees.js.jsx
+++ app/assets/javascripts/components/employees.js.jsx
@@ -1,9 +1,37 @@
 var Employees = React.createClass({
 
+	getInitialState() {
+    return {
+			// in order to hire an employee, create object for new employee
+      employees: this.props.employees,
+      employee: {
+        name: '',
+        email: '',
+        manager: false
+      },
+      errors: {}
+    }
+  },
+
   render () {
     employees = this.state.employees.map( function(employee) {
       return (
         <Employee employee={employee} key={employee.id} />
+ 				<tbody>
+        {employees}
+        <tr>
+          <td>
+            <input type="text" onChange={this.handleNameChange} /><br />
+            <span style={{color: 'red'}}>{this.state.errors.name}</span>
+          </td>
+          <td>
+            <input type="text" onChange={this.handleEmailChange} /><br />
+            <span style={{color: 'red'}}>{this.state.errors.email}</span>
+          </td>
+          <td><input type="checkbox" onChange={this.handleManagerChange} /></td>
+          <td><button onClick={this.handleHireEmployee}>Hire</button></td>
+        </tr>
+      </tbody> 
       );
     });
     return (
