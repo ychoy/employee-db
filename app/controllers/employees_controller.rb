8846e6a8b993653da889e86c610b343ea9bd931c
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-07-11 20:21:08 -0700

    add create action to respond with new employee

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -3,5 +3,21 @@ class EmployeesController < ApplicationController
     @employees = Employee.all
     render component: 'Employees', props: { employees: @employees } #render react component to list all employees
   end
+	
+	 def create
+    @employee = Employee.new(employee_params)
+    respond_to do |format|
+      format.json do 
+        if @employee.save
+          render :json => @employee
+        else
+          render :json => { :errors => @employee.errors.messages }, :status => 422
+        end
+      end
+    end
+  end
+
+
+
 end  
 
