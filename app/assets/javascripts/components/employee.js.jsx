33596700310b952d662f7fdd5f05585d9b1e0ea8
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-07-11 20:47:34 -0700

    update action for editing employees

--- app/assets/javascripts/components/employee.js.jsx
+++ app/assets/javascripts/components/employee.js.jsx
@@ -1,17 +1,99 @@
 var Employee = React.createClass({
-  getInitialState() {
-    return {
-      employee: this.props.employee // pass employee object as a prop to component 
-    }
+	getInitialState() {
+  	return {
+   		employee: this.props.employee // pass employee object as a prop to component 
+   		editMode: false,
+   		errors: {}
+   	}
+ 	},
+
+	setEditMode() {
+    this.setState({editMode: true});
+  },
+
+  handleNameChange(e) {
+    var newEmployee = this.state.employee;
+    newEmployee.name = e.target.value;
+    this.setState({employee: newEmployee});
+  },
+
+  handleEmailChange(e) {
+    var newEmployee = this.state.employee;
+    newEmployee.email = e.target.value;
+    this.setState({employee: newEmployee});
+  },
+
+  handleManagerChange(e) {
+    var newEmployee = this.state.employee;
+    newEmployee.manager = e.target.value;
+    this.setState({employee: newEmployee});
+  },
+
+	handleEmployeeUpdate() {
+    var that = this;
+    $.ajax({
+      method: 'PUT',
+      data: {
+        employee: that.state.employee,
+      },
+      url: '/employees/' + that.state.employee.id + '.json',
+      success: function(res) {
+        that.setState({
+          errors: {},
+          employee: res,
+          editMode: false
+        });
+      },
+      error: function(res) {
+        that.setState({errors: res.responseJSON.errors});
+      }
+    });
   },
 
   render() {
-    return (
-      <tr>
-        <td>{this.state.employee.name}</td>
-        <td>{this.state.employee.email}</td>
-        <td>{this.state.employee.manager ? '✔' : ''}</td>
-      </tr>
-    );
+    if ( this.state.editMode ) {
+      markup = (
+        <tr>
+          <td>
+            <input
+              type="text"
+              value={this.state.employee.name}
+              onChange={this.handleNameChange} />
+            <span style={{color: 'red'}}>{this.state.errors.name}</span>
+          </td> 
+          <td>
+            <input
+              type="text"
+              value={this.state.employee.email}
+              onChange={this.handleEmailChange} />
+            <br />
+            <span style={{color: 'red'}}>{this.state.errors.email}</span>
+          </td>
+          <td>
+            <input
+              type="checkbox"
+              value={this.state.employee.manager}
+              onChange={this.handleManagerChange} />
+          </td>
+          <td>
+            <button onClick={this.handleEmployeeUpdate}>Save</button>
+          </td>
+        </tr>
+      );
+    } else {
+      markup = (
+        <tr>
+          <td>{this.state.employee.name}</td>
+          <td>{this.state.employee.email}</td>
+          <td>{this.state.employee.manager ? '✔' : ''}</td>
+          <td>
+            <button onClick={this.setEditMode}>Edit</button>
+          </td>
+        </tr>
+      );
+    }
+    return markup;
   }
+
+
 });
