4e219552897a87bc7d9892889728c54cac3de6dc
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-07-11 20:17:09 -0700

    update employees component to render Employee component

--- app/assets/javascripts/components/employees.js.jsx
+++ app/assets/javascripts/components/employees.js.jsx
@@ -1,13 +1,9 @@
 var Employees = React.createClass({
 
-  render: function() {
-    employees = this.props.employees.map( function(employee) {
+  render () {
+    employees = this.state.employees.map( function(employee) {
       return (
-        <tr key={employee.id}>
-          <td>{employee.name}</td>
-          <td>{employee.email}</td>
-          <td>{employee.manager ? '&#10004;' : ''}</td>
-        </tr>
+        <Employee employee={employee} key={employee.id} />
       );
     });
     return (
