6b15fd29ab9c449e2a649cdb238340730c762a30
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-07-11 18:41:27 -0700

    moving employee into react component

--- app/assets/javascripts/components/employees.js.jsx
+++ app/assets/javascripts/components/employees.js.jsx
@@ -1,33 +1,27 @@
 var Employees = React.createClass({
 
-  render: function() {
-    employees = this.props.employees.map( function(employee) {
-      return (
-        <tr key={employee.id}>
-          <td>{employee.name}</td>
-          <td>{employee.email}</td>
-          <td>{employee.manager ? '&#10004;' : ''}</td>
-        </tr>
-      );
-    });
+// pass employee object as prop to component 
+  getInitialState() {
+    return {
+      employee: this.props.employee
+    }
+  },
+
+	render() {
     return (
-      <div>
-        <h1>Employees</h1>
-        <div id="employees">
-          <table>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Manager</th>
-              </tr>
-            </thead>
-            <tbody>
-              {employees}
-            </tbody>
-          </table>
-        </div>
-      </div>
+      <tr>
+        <td>{this.state.employee.name}</td>
+        <td>{this.state.employee.email}</td>
+        <td>{this.state.employee.manager ? '✔' : ''}</td>
+      </tr>
     );
   }
-});
+
+// render Employee component 
+  render () {
+    employees = this.state.employees.map( function(employee) {
+      return (
+        <Employee employee={employee} key={employee.id} />
+      );
+   });
+};
